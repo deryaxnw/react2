2931ec043eca4646ef2b4bfaa006d66cacc99ef7
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-01-20 10:07:26 -0300

    criando meu hook, item favorito do user

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,37 @@
 // import { useRef, useState } from "react";
 // import { useMemo, useState , useContext} from "react";
 import "./App.css";
-import { Button } from "./components/Contexto/Button";
-import { Produto } from "./components/Contexto/Produto";
-import { StorageContexto } from "./components/Contexto/StorageContexto";
+// import { Button } from "./components/Contexto/Button";
+// import { Produto } from "./components/Contexto/Produto";
+// import { StorageContexto } from "./components/Contexto/StorageContexto";
 // import { Carrinho } from "./components/useRef/Carrinho";
 // import Ref from "./components/useRef/Ref";
 
 // import { Produto } from "./components/userContext/Produto";
 // import { StorageContext } from "./components/userContext/UserContext";
+import useLocalStorage from './components/meuHook/useLocalStorage'
 
 function App() {
+  const [produto , setProduto] = useLocalStorage('produto', '')
+
+  function handleClick({target}) {
+    setProduto(target.innerText)
+  }
+
   return (
     <>
-      <StorageContexto>
+
+      <p>Produto preferido: {produto}</p>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartPhone</button>
+
+
+
+
+      {/* <StorageContexto>
         <Produto />
         <Button />
-      </StorageContexto>
+      </StorageContexto> */}
     </>
   );
 }
